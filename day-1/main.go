42b13e0fc7811340113f604209bcb2669a8a86ab
Shane Welsh <shanemwelsh@gmail.com>
2023-12-01 20:07:49 -0500

    Day 1 Part 2

--- day-1/main.go
+++ day-1/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 	"unicode"
 )
 
@@ -16,9 +18,8 @@ func main() {
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-        result += calibration_value(scanner.Text())
+        result += calibration_value(replace_word_digits(scanner.Text()))
     }
-
     check(scanner.Err())
 
    fmt.Println(result) 
@@ -51,4 +52,38 @@ func calibration_value(line string) int {
     }
 
     return a * 10 + b
+}
+
+var number_map = map[string] string {
+    "one": "o1ne",
+    "two": "t2wo",
+    "three": "t3hree",
+    "four": "f4our",
+    "five": "f5ive",
+    "six": "s6ix",
+    "seven": "s7even",
+    "eight": "e8ight",
+    "nine": "n9ine",
+}
+
+func replace_word_digits(input string) string {
+    sub_pattern := "one|two|three|four|five|six|seven|eight|nine"
+    r_first := regexp.MustCompile(fmt.Sprintf("(%s).*", sub_pattern))
+    r_last := regexp.MustCompile(fmt.Sprintf(".*(%s)", sub_pattern))
+
+    result := input
+    first_matches := r_first.FindStringSubmatch(result)
+    last_matches := r_last.FindStringSubmatch(result)
+
+    if len(first_matches) > 0 {
+        first := first_matches[1]
+        result = strings.Replace(result, first, number_map[first], 1)
+    }
+
+    if len(last_matches) > 0 {
+        last := last_matches[1]
+        result = strings.ReplaceAll(result, last, number_map[last])
+    }
+
+    return result
 }
